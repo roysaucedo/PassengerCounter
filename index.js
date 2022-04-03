36d5421abaa051b5652ea9fed24b3ab01baf0972
roysaucedo <roy.a.saucedo@gmail.com>
2022-04-03 11:42:32 -0700

    First Build

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-// document.getElementById("count-el").innerText = 5
-
-// change the count-el in the HTML to reflect the new count
-
-// camelCase
 let countEl = document.getElementById("count-el")
-
-console.log(countEl)
-
 let count = 0
 
+let saveEl = document.getElementById("save-el")
+
+console.log(saveEl)
+
 function increment() {
-    count = count + 1
-    countEl.innerText = count
+    count += 1
+    countEl.textContent = count
+    console.log(count)
+}
+
+function save() {
+    let countStr = count + " -"
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
 }
